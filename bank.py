f571d97ffd2a0e9f6cf03b9a550ff767cdcc47dc
DavoudTaghawiNejad <davoud@taghawi-nejad.de>
2018-05-08 22:55:32 +0100

    Single Round ABM

--- bank.py
+++ bank.py
@@ -1,26 +1,32 @@
 import abcFinance
+from accountingsystem import s
+from random import random
 
 
 class Bank(abcFinance.Agent):
-    def init(self, reserves=0, loans=0, deposits=0):
+    def init(self, min_capital_ratio, reserves=0, loans=0, deposits=0):
+        self.min_capital_ratio = min_capital_ratio
         self.make_asset_accounts(['reserves', 'loans'])
         self.make_liability_accounts(['deposits'])
         self.make_flow_accounts(['interest income'])
-        #self.make_residual_account('equity')
+
         self.book(debit=[('reserves', reserves), ('loans', loans)],
                   credit=[('deposits', deposits), ('equity', reserves + loans - deposits)],
                   text='Capital endowment')
         self.book_end_of_period()
 
-    def grant_loan(self):
-        _, cash = self.accounts.get_balance('cash')
-        _, deposits = self.accounts.get_balance('deposits')
-        available_deposits = max(0, cash / 0.1 - deposits)
+    def grant_loans(self):
+        side_equity, equity = self.accounts.get_balance('equity')
+        total_assets = self.accounts.get_total_assets()
         offers = self.get_messages('request_loan')
-        for offer in offers:
-            amount = offer.content
-            if amount <= available_deposits:
-                self.book(debit=[(what, amount)],
-                          credit=[('', amount)],
-                         text="buy %s" % what)
+        if side_equity != s.DEBIT:
+            for offer in offers:
+                amount = offer.content
+                if equity / (total_assets + amount) >= self.min_capital_ratio:
+                    self.book(debit=[('loans', amount)], credit=[('deposits', amount)], text='Loan granting')
 
+                    self.send(offer.sender, '_autobook', dict(debit=[('money', amount)],
+                                                              credit=[('loans', amount)],
+                                                              text='Take out loan'))
+
+                    self.send(offer.sender, 'interest_rate', random() * 0.1)
--- household.py
+++ household.py
@@ -1,4 +1,5 @@
 import abcFinance
+from random import randrange
 
 
 class Household(abcFinance.Agent):
@@ -6,19 +7,41 @@ class Household(abcFinance.Agent):
         self.make_asset_accounts(['money','participations'])
         self.make_liability_accounts(['loans'])
         self.make_flow_accounts(['dividend income','interest expenses'])
-        #self.make_residual_account('equity')
-        self.book(debit=[('money', money),('participations', participations)],
+        # self.make_residual_account('equity')
+        self.book(debit=[('money', money), ('participations', participations)],
                   credit=[('loans', loans), ('equity', money + participations - loans)],
                   text='Initial endowment')
         self.book_end_of_period()
 
-    def deposit_money(self, bank, amount):
-        self.send(bank, '_autobook', dict(debit=[('cash', amount)],
-                                          credit=[('deposits', amount)],
-                                          text='deposit cash'))
-        self.book(debit=[('deposits', amount)],
-                  credit=[('cash', amount)],
-                  text='deposits cash')
+    def request_loans(self):
+        bank = ('bank', 0)
+        self.send(bank, 'request_loan', randrange(10, 100))
 
-    def get_loan(self, bank, amount):
-        self.send(bank, 'request_loan', amount)
+    def pay_interest(self):
+        bank = ('bank', 0)
+        interest_message = self.get_messages('interest_rate')
+        if interest_message:
+            interest_rate = interest_message[0].content
+            self.interest_rate = interest_rate
+        else:
+            interest_rate = self.interest_rate
+        _, loan = self.accounts['loans'].get_balance()
+        interest_payment = loan * interest_rate
+        self.book(debit=[('interest expenses', interest_payment)],
+                  credit=[('money', interest_payment)],
+                  text='Interest payment')
+
+        self.send(bank, '_autobook', dict(
+            debit=[('deposits', interest_payment)],
+            credit=[('interest income', interest_payment)],
+            text='Interest payment'))
+
+    def repay_loan_principal(self):
+        bank = ('bank', 0)
+        _, loan = self.accounts['loans'].get_balance()
+
+        self.book(debit=[('loans', loan)], credit=[('money', loan)], text='Principal repayment')
+        self.send(bank, '_autobook', dict(
+            debit=[('deposits', loan)],
+            credit=[('loans', loan)],
+            text='Principal repayment'))
